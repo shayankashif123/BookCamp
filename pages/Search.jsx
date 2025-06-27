62d464a931287eb282add46361962dd07d768d4a
Shayan Kashif <shayanfarooqui789@gmail.com>
2025-06-27 19:13:15 +0500

    Update Search.jsx

--- pages/Search.jsx
+++ pages/Search.jsx
@@ -1,4 +1,3 @@
-import React, { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { fetchData } from '../redux/bookSlice';
